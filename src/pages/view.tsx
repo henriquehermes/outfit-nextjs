6aabc5ca440ba2c3dc7a265baeda75554e9f1eb9
Henrique <henriqueh.97@hotmail.com>
2022-06-04 14:26:41 -0300

    feat: view page

---
+++ src/pages/view.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import ViewScreen from "../screens/View";
+
+const ViewPage: NextPage = () => {
+    return <ViewScreen />;
+};
+
+export default ViewPage;
---
+++ src/screens/View/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { ContainerCustom } from "../../components/Container";
+
+const ViewPage: FC = () => {
+    return (
+        <ContainerCustom>
+            <div></div>
+        </ContainerCustom>
+    );
+};
+
+export default ViewPage;
